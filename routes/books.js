b73f442e34aa47e5de36edb3bd6aeed95562ec9a
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-01-20 23:03:46 -0600

    adjust and updated routes

--- routes/books.js
+++ routes/books.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var Book = require('../models').Book;
 
+/* wraps each route function callback */
 function asyncHandler(callback) {
   return async (req, res, next) => {
     try {
       await callback(req, res, next);
     } catch (error) {
       console.error('Something happened: ' + error);
+      res.status(500).render('error', {headTitle: 'Server Error'});
     }
   }
 }
@@ -15,23 +17,39 @@ function asyncHandler(callback) {
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res, next) => {
     const books = await Book.findAll();
-    res.render("index", {title: 'Books', books: books});
+    res.render("index", {headTitle: 'Books', title: 'Library', books: books});
 }));
 
 /* GET new book form page. */
 router.get('/new', asyncHandler(async (req, res, next) => {
-  res.render('new-book', {title: 'New Book'});
+  res.render("new-book", {headTitle: 'New Book', title: 'New Book', newBook: {}});
 }));
 
 /* POST new book. */
 router.post('/new', asyncHandler(async (req, res, next) => {
-  const newBook = await Book.create(req.body);
+  let newBook;
+  try {
+    newBook = await Book.create(req.body);
+    res.redirect('/');
+  }
+  catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      newBook = await Book.build(req.body);
+      res.render('new-book', {title: 'New Book', errors: error.errors, newBook: newBook});
+    } else {
+      throw error;
+    }
+  }
 }));
 
 /* GET book by id. */
 router.get('/:id', asyncHandler(async (req, res, next) => {
   const book = await Book.findByPk(req.params.id);
-  res.render('detail', {title: 'Update book', book: book});
+  try {
+    res.render('update-book', {headTitle: book.title, title: 'Update Book', book: book});
+  } catch (error) {
+    throw error;
+  }
 }));
 
 /* UPDATE book by id. */
